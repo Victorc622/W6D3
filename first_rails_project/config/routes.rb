b5da38cadfb614fdfea856f5e2d27fd8dcb8771e
Edison Li <47466335+edison4354@users.noreply.github.com>
2024-04-10 12:38:32 -0700

    Add RESTful convention routes

--- first_rails_project/config/routes.rb
+++ first_rails_project/config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # resources :users
+
+  get 'users/', to: 'users#index', as: 'users'
+  post 'users/', to: 'users#create'
+  get 'users/:id', to: 'users#show', as: 'user'
+  patch 'users/:id', to: 'users#update'
+  put 'users/:id', to: 'users#update'
+  delete 'users/:id', to: 'users#destroy'
+
 end
